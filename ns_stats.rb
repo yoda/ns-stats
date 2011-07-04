ca4875209c10992a4b3a3c9d993fb69eab779c40
yoda <yoda@tallon4.net>
2011-07-04 21:39:34 +0800

    Minor validation

--- ns_stats.rb
+++ ns_stats.rb
@@ -4,6 +4,8 @@ require 'rest-client'
 require 'json'
 
   QUERY_URL = "http://unknownworldsstats.appspot.com/statkilldata"
+  VALID_BUILD_NUMBERS = (175..200).map{|n| n.to_s}
+  VALID_MAP_NAMES = ['ns2_summit', 'ns2_rockdown', 'ns2_junction', 'ns2_tram' ]
 
   get '/' do
     haml :index
@@ -11,9 +13,11 @@ require 'json'
 
   get '/:build_number/:map_name' do |build_number, map_name|
     content_type :json
-    begin 
-      response =  RestClient.get QUERY_URL, {:params => {:version => build_number, :map => map_name}}
-    rescue => e
-      e.response
+    if VALID_BUILD_NUMBERS.include?(build_number) && VALID_MAP_NAMES.include?(map_name)
+      begin 
+        response =  RestClient.get QUERY_URL, {:params => {:version => build_number, :map => map_name}}
+      rescue => e
+        e.response
+      end
     end
   end
