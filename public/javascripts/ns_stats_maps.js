1b762827821e4aa6abb873ed1463dceeda1129b6
yoda <yoda@tallon4.net>
2011-07-23 15:59:19 +0800

    Working on getting the scaling correct...

--- public/javascripts/ns_stats_maps.js
+++ public/javascripts/ns_stats_maps.js
@@ -1,18 +1,78 @@
+var Point = function(_x,_y,_z) {
+  var x = _x;
+  var y = _y;
+  var z = _z;
+
+  this.x = x;
+  this.y = y;
+  this.z = z;
+};
+
+MAP_METADATA = [
+  {
+    'map_name': 'ns2_summit',
+    'scale': new Point(414.368, 269.451, 414.368),
+    'origin': new Point(36.0886, -42.1639, 59.3301),
+  }
+];
+
+IMAGE_SIZE = 256;
+
+function metadata_for_map(map_name) {
+  var meta_data = {};
+  $.each(MAP_METADATA, function(index, val) {
+    if (val['map_name'] == map_name) {
+      meta_data = val;
+    }
+  });
+  return meta_data;
+};
+
+
+function kMapRatio(map_metadata) {
+  if (map_metadata['scale'].z > map_metadata['scale'].x) {
+    return map_metadata['scale'].z / map_metadata['scale'].x;
+  } else {
+    return map_metadata['scale'].x / map_metadata['scale'].z;
+  }
+}
+
+function plotToMap(posX, posZ, map_metadata) {
+
+    var adjustedX = posX - map_metadata['origin'].x;
+    var adjustedZ = posZ - map_metadata['origin'].z;
+    
+    var xFactor = 4;
+    var zFactor = xFactor / kMapRatio(map_metadata);
+
+    var plottedX = (adjustedX / (map_metadata['origin'].x / xFactor)) * (12);
+    var plottedY = (adjustedZ / (map_metadata['origin'].z / zFactor)) * (18);
+
+    plottedX += (256 / 2);
+    plottedY += (256 / 2);
+
+    // The world space is oriented differently from the GUI space, adjust for that here.
+    return {'x': plottedX, 'y': plottedY};
+}
+
+
 // prefix must be the prefix of the variable type eg. "attacker" is the prefix of "attackerx" or "attacker_team"
-function draw_heatmap(data, bg, prefix, team_select) {
+function draw_heatmap(data, bg, prefix, team_select, map_name) {
   var aliens = (team_select == ALIEN_TEAM);
   var marines = (team_select == MARINE_TEAM);
   var both = (team_select == BOTH_TEAM);
-  var scale = 1.26;
-  var offsetx = 52;
-  var offsety = 82;
+  var map_metadata = metadata_for_map(map_name);
   $.each(data, function(index, val) {
+
+    var map_point = plotToMap(val[(prefix + "x")], val[(prefix + "z")], map_metadata);
+
+
     if ((val[prefix + "_team"] == 1) && (marines || both)) {
-      var c = bg.circle((val[(prefix + "z")]*scale) + offsetx, (val[(prefix + "x")]*scale) + offsety, 2);
+      var c = bg.circle(map_point['x'], map_point['y'], 2);
       c.attr({fill: "blue"});
     }
     if ((val[prefix + "_team"] == 2) && (aliens || both)) {
-      var c = bg.circle((val[(prefix + "z")]*scale) + offsetx, (val[(prefix + "x")]*scale) + offsety, 2);
+      var c = bg.circle(map_point['x'], map_point['y'], 2);
       c.attr({fill: "green"});
     }
 
@@ -32,6 +92,6 @@ function new_heatmap_canvas(height, width, container_element, title, map_name) {
 
 
 function create_heatmap(query, map_name, data, target) {
-    var a_bg = new_heatmap_canvas(256, 256, target, query[2], map_name);
-    draw_heatmap(data, a_bg, query[0], query[1]);
+    var a_bg = new_heatmap_canvas(IMAGE_SIZE, IMAGE_SIZE, target, query[2], map_name);
+    draw_heatmap(data, a_bg, query[0], query[1], map_name);
 }
